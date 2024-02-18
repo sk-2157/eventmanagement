36aead5d6fcc43cb71e19c6654df451bb531c74c
sk-2157 <shubhamraje5770@gmail.com>
2024-02-18 20:30:32 +0530

    image capture

--- eventmanagement/participant/views.py
+++ eventmanagement/participant/views.py
@@ -1,17 +1,28 @@
+import base64
 from django.shortcuts import render, redirect
 from .models import Participant
 from django.contrib import messages 
+from django.core.files.base import ContentFile
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 # Create your views here.
+@csrf_exempt
 def participant(request):
     if request.method == 'POST':
         event_id = request.POST['event_id']
         event = request.POST['event']
         name = request.POST['name']
         email = request.POST['email']
-        phone = request.POST['phone']
-        message = request.POST['message']
+        #phone = request.POST['phone']
         user_id = request.POST['user_id']
+        captured_image_data_url = request.POST.get('captured_image')
+        captured_image_path = f'media/{event}/{name}.png'
+        with open(captured_image_path, 'wb') as destination:
+            destination.write(base64.b64decode(captured_image_data_url.split(',')[1]))
+
+
 
         if request.user.is_authenticated:
             user_id = request.user.id
@@ -20,7 +31,7 @@ def participant(request):
                 messages.error(request, 'You have already registered for the event')
                 return redirect('/events/' + event_id)
 
-        participant = Participant(event_id=event_id,event=event,name=name,email=email,phone=phone,message=message,user_id=user_id)
+        participant = Participant(event_id=event_id,event=event,name=name,email=email,user_id=user_id,p_photo=captured_image_path)
         participant.save()
 
         messages.success(request, 'You have successfully registered for the event')
